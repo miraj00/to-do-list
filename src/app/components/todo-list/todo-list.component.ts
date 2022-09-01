755b8df76e745fe9865633e2504fac1fc9417728
miraj <miraj00@gmail.com>
2022-08-31 22:21:28 -0400

    interface imported in todo-list component

--- src/app/components/todo-list/todo-list.component.ts
+++ src/app/components/todo-list/todo-list.component.ts
@@ -1,11 +1,13 @@
 import { Component, OnInit } from '@angular/core';
-
+import { Todo } from 'src/app/interface/todo';
 @Component({
   selector: 'app-todo-list',
   templateUrl: './todo-list.component.html',
   styleUrls: ['./todo-list.component.css']
 })
 export class TodoListComponent implements OnInit {
+
+  todoList: Todo[] = [];
 
   constructor() { }
 
